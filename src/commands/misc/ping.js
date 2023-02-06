97e00e45a4abec8699268f8ee4fc85b9e07df46e
Kye-Browning <kye.browning@gmail.com>
2023-02-05 18:43:39 -0600

    Initial commit

---
+++ src/commands/misc/ping.js
@@ -0,0 +1,13 @@
+module.exports = {
+  deleted: true,
+  name: 'ping',
+  description: 'Pong!',
+  // devOnly: Boolean,
+  // testOnly: true,
+  // options: Object[],
+  
+
+  callback: (client, interaction) => {
+    interaction.reply(`Pong! ${client.ws.ping}ms`);
+  },
+};
---
+++ src/commands/voidHorizon/units.js
@@ -0,0 +1,48 @@
+module.exports = {
+    name: 'vh',
+    description: 'Rules and Unit lookup for Void Horizon',
+    options: [
+        {
+            name: 'unit',
+            description: 'Units SubGroup',
+            type: 2,
+            options: [
+                {
+                    name: 'rabble',
+                    description: 'Rabble Base Stats',
+                    type: 1,
+                },
+                {
+                    name: 'light-infantry',
+                    description: 'Light Infantry Base Stats',
+                    type: 1,
+                },
+                {
+                    name: 'heavy-infantry',
+                    description: 'Heavy Infantry Base Stats',
+                    type: 1,
+                },
+                {
+                    name: 'lav',
+                    description: 'Light Armored Vehicle Base Stats',
+                    type: 1,
+                },
+                {
+                    name: 'hav',
+                    description: 'Heavy Armored Vehicle Base Stats',
+                    type: 1,
+                },
+                {
+                    name: 'war-behemoth',
+                    description: 'War Behemoth Base Stats',
+                    type: 1,
+                },
+
+            ],
+
+        },
+    ],
+
+    
+};
+  
